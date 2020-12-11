9c3584fb422797ac2927de4746b632ba44e02d5b
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-12-10 21:18:45 -0500

    Fixing first key like 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,12 +6,12 @@ Determines if all the boxes can be opened
 
 def canUnlockAll(boxes):
     """ Check boxes """
-    keys = boxes[0]
+    keys = [0]
     for k in keys:
         openbox = boxes[k]
         for nk in openbox:
             if nk not in keys and nk > 0:
                 keys.append(nk)
-    if len(keys) == len(boxes) - 1:
+    if len(keys) == len(boxes):
         return True
     return False
